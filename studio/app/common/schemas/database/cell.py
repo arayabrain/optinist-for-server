15e14973436b4b93316373d830f8260256f9f75c
quanpython <quannd@reactplus.jp>
2023-07-07 15:59:13 +0700

    add database schemas

---
+++ studio/app/common/schemas/database/cell.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+from studio.app.common.schemas.base import Pagenation
+from studio.app.common.schemas.database.experiment import (
+    DbExperimentFields,
+    DbExperimentHeader,
+)
+
+
+class DbCell(BaseModel):
+    id: int
+    exp_id: int
+    fields: DbExperimentFields
+    graph_urls: List[str]
+    created_time: Optional[datetime]
+    updated_time: Optional[datetime]
+
+
+class DbCells(BaseModel):
+    pagenation: Pagenation
+    header: DbExperimentHeader
+    records: List[DbCell]
