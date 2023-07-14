c5414a62f9e5adc2d17b4dc5a61c8850982a239c
quanpython <quannd@reactplus.com>
2023-07-14 16:25:07 +0700

    implement expdb router

--- studio/app/optinist/routers/expdb.py
+++ studio/app/optinist/routers/expdb.py
@@ -1,14 +1,24 @@
 from typing import Optional
 
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Depends, HTTPException, Query
+from fastapi_pagination.ext.sqlmodel import paginate
+from sqlmodel import Session, select
 
+from studio.app.common import models as common_model
+from studio.app.common.core.auth.auth_dependencies import get_current_user
+from studio.app.common.db.database import get_db
+from studio.app.common.schemas.users import UserInfo
+from studio.app.optinist import models as optinist_model
 from studio.app.optinist.schemas.base import SortOptions
-from studio.app.optinist.schemas.expdb.cell import ExpDbCells
+from studio.app.optinist.schemas.expdb.cell import ExpDbCell
 from studio.app.optinist.schemas.expdb.experiment import (
-    ExpDbExperiments,
+    ExpDbExperiment,
+    ExpDbExperimentFields,
+    ExpDbExperimentHeader,
     ExpDbExperimentSharePostStatus,
     ExpDbExperimentShareStatus,
     ExpDbExperimentsSearchOptions,
+    PageWithHeader,
     PublishFlags,
 )
 
@@ -17,20 +27,20 @@ router = APIRouter()
 
 @router.get(
     "/public/experiments",
-    response_model=ExpDbExperiments,
+    response_model=PageWithHeader[ExpDbExperiment],
     description="* 公開 Experiments を検索し、結果を応答",
 )
 async def search_public_experiments(
     options: ExpDbExperimentsSearchOptions = Depends(),
     sortOptions: SortOptions = Depends(),
+    db: Session = Depends(get_db),
 ):
-    return search_db_experiments(options, sortOptions)
+    return await search_db_experiments(db, options, sortOptions)
 
 
 @router.get(
     "/public/cells",
-    response_model=ExpDbCells,
-    summary=None,
+    response_model=PageWithHeader[ExpDbCell],
     description="""
 - 公開 Cells を検索し、結果を応答
 """,
@@ -38,76 +48,153 @@ async def search_public_experiments(
 async def search_public_cells(
     exp_id: Optional[int] = Query(None, description="experiments.id"),
     options: ExpDbExperimentsSearchOptions = Depends(),
-    query_param: SortOptions = Depends(),
+    sortOptions: SortOptions = Depends(),
+    db: Session = Depends(get_db),
 ):
-    return
+    data = paginate(
+        session=db,
+        query=select(optinist_model.Cell).filter(
+            optinist_model.Cell.experiment_seqid == exp_id
+        ),
+        additional_data={"header": ExpDbExperimentHeader(graph_titles=[])},
+    )
+    for item in data.items:
+        item.fields = ExpDbExperimentFields(
+            brain_area=0, cre_driver=0, reporter_line=0, imaging_depth=0
+        )
+    return data
 
 
 @router.get(
     "/expdb/experiments",
-    response_model=ExpDbExperiments,
-    summary=None,
+    response_model=PageWithHeader[ExpDbExperiment],
+    dependencies=[Depends(get_current_user)],
     description="""
 - Experiments を検索し、結果を応答
 """,
 )
 async def search_db_experiments(
+    db: Session = Depends(get_db),
     options: ExpDbExperimentsSearchOptions = Depends(),
     sortOptions: SortOptions = Depends(),
 ):
-    return
+    data = paginate(
+        session=db,
+        query=select(optinist_model.Experiment),
+        additional_data={"header": ExpDbExperimentHeader(graph_titles=[])},
+    )
+    for item in data.items:
+        item.fields = ExpDbExperimentFields(
+            brain_area=0, cre_driver=0, reporter_line=0, imaging_depth=0
+        )
+    return data
 
 
 @router.get(
     "/expdb/cells",
-    response_model=ExpDbCells,
-    summary=None,
+    response_model=PageWithHeader[ExpDbCell],
+    dependencies=[Depends(get_current_user)],
     description="""
 - Cells を検索し、結果を応答
 """,
 )
 async def search_db_cells(
+    db: Session = Depends(get_db),
     exp_id: Optional[int] = Query(None, description="experiments.id"),
     options: ExpDbExperimentsSearchOptions = Depends(),
     sortOptions: SortOptions = Depends(),
 ):
-    return
+    data = paginate(
+        session=db,
+        query=select(optinist_model.Cell).filter(
+            optinist_model.Cell.experiment_seqid == exp_id
+        ),
+        additional_data={"header": ExpDbExperimentHeader(graph_titles=[])},
+    )
+    return data
 
 
 @router.post(
     "/expdb/experiment/publish/{id}/{flag}",
     response_model=bool,
-    summary=None,
+    dependencies=[Depends(get_current_user)],
     description="""
 - Experiments を公開する
 
 """,
 )
-async def publish_db_experiment(id: int, flag: PublishFlags):
-    return 1
+async def publish_db_experiment(
+    id: int,
+    flag: PublishFlags,
+    db: Session = Depends(get_db),
+):
+    exp = (
+        db.query(optinist_model.Experiment)
+        .filter(optinist_model.Experiment.id == id)
+        .first()
+    )
+    if not exp:
+        raise HTTPException(status_code=404)
+    exp.publish_status = int(flag == PublishFlags.on)
+    db.add(exp)
+    db.commit()
+    return True
 
 
 @router.get(
     "/expdb/share/{id}/status",
     response_model=ExpDbExperimentShareStatus,
-    summary=None,
+    dependencies=[Depends(get_current_user)],
     description="""
 - Experiment Database の共有状態を取得する
 """,
 )
-def get_experiment_database_share_status(id: int):
-    return
+def get_experiment_database_share_status(
+    id: int,
+    db: Session = Depends(get_db),
+):
+    exp = (
+        db.query(optinist_model.Experiment)
+        .join(
+            common_model.Organization,
+            common_model.Organization.id == optinist_model.Experiment.organization_id,
+        )
+        .filter(optinist_model.Experiment.id == id)
+        .first()
+    )
+    if not exp:
+        raise HTTPException(status_code=404)
+    users = (
+        db.query(common_model.User)
+        .filter(common_model.User.organization_id == exp.organization_id)
+        .all()
+    )
+    return ExpDbExperimentShareStatus(
+        share_type=exp.share_type, users=[UserInfo.from_orm(user) for user in users]
+    )
 
 
 @router.post(
     "/expdb/share/{id}/status",
     response_model=bool,
-    summary=None,
+    dependencies=[Depends(get_current_user)],
     description="""
 - Experiment Database の共有状態を更新する（総入れ替え）
 """,
 )
 def update_experiment_database_share_status(
-    id: int, data: ExpDbExperimentSharePostStatus
+    id: int,
+    data: ExpDbExperimentSharePostStatus,
+    db: Session = Depends(get_db),
 ):
+    exp = (
+        db.query(optinist_model.Experiment)
+        .filter(optinist_model.Experiment.id == id)
+        .first()
+    )
+    if not exp:
+        raise HTTPException(status_code=404)
+    exp.share_type = data.share_type
+    db.add(exp)
+    db.commit()
     return True
