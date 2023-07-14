79de6242c9ea7905bc9a8336d4ada5650883f14c
quanpython <quannd@reactplus.com>
2023-07-14 16:09:37 +0700

    add fastapi-pagination

--- studio/__main_unit__.py
+++ studio/__main_unit__.py
@@ -4,6 +4,7 @@ import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi_pagination import add_pagination
 from starlette.middleware.cors import CORSMiddleware
 
 from studio.app.common.routers import (
@@ -21,6 +22,8 @@ from studio.app.dir_path import DIRPATH
 from studio.app.optinist.routers import expdb, hdf5, nwb, roi
 
 app = FastAPI(docs_url="/docs", openapi_url="/openapi")
+
+add_pagination(app)
 
 # common routers
 app.include_router(algolist.router)
