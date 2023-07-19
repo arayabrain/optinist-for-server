9d1abba96ff1c12de2211ae422474f3c995976f9
ReiHashimoto <42664619+ReiHashimoto@users.noreply.github.com>
2023-07-19 15:49:59 +0900

    add layout for public pages

---
+++ frontend/src/pages/PublicDatabase/index.tsx
@@ -0,0 +1,12 @@
+import PublicLayout from "components/PublicLayout"
+import Database from "pages/Database"
+
+const PublicDatabase = () => {
+  return(
+    <PublicLayout>
+      <Database />
+    </PublicLayout>
+  )
+}
+
+export default PublicDatabase
