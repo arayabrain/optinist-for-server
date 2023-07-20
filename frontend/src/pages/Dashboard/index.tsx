414a44085d1e372fd39bbd6e7864d07befa6546e
tienday <>
2023-07-20 14:31:04 +0900

    adjust links.

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom'
 import { Box, styled, Typography } from '@mui/material'
 import StorageIcon from '@mui/icons-material/Storage'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle'
+import AnalyticsIcon from '@mui/icons-material/Analytics'
 
 const Dashboard = () => {
   return (
@@ -12,23 +13,15 @@ const Dashboard = () => {
           <LinkWrapper to="/console/experiments">
             <BoxMenu>
               <Box>
-                <AccountCircleIcon fontSize="large" />
+                <StorageIcon fontSize="large" />
                 <TitleMenu>Database</TitleMenu>
-              </Box>
-            </BoxMenu>
-          </LinkWrapper>
-          <LinkWrapper to="/">
-            <BoxMenu>
-              <Box>
-                <AccountCircleIcon fontSize="large" />
-                <TitleMenu>Database public<br /><span style={{fontSize: "medium"}}>(for development)</span></TitleMenu>
               </Box>
             </BoxMenu>
           </LinkWrapper>
           <LinkWrapper to="/console/workspaces">
             <BoxMenu>
               <Box>
-                <StorageIcon fontSize="large" />
+                <AnalyticsIcon fontSize="large" />
                 <TitleMenu>Workspaces</TitleMenu>
               </Box>
             </BoxMenu>
