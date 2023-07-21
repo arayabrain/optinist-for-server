fe727afdad809b0f9267ebae54bbca19f807a380
quanpython <quannd@reactplus.com>
2023-07-21 15:37:51 +0700

    account management, authentication with db

--- studio/app/common/routers/users_me.py
+++ studio/app/common/routers/users_me.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter, Depends
+from sqlmodel import Session
 
 from studio.app.common.core.auth.auth_dependencies import get_current_user
 from studio.app.common.core.users import crud_users
-from studio.app.common.schemas.users import User, UserPasswordUpdate, UserUpdate
+from studio.app.common.db.database import get_db
+from studio.app.common.schemas.users import SelfUserUpdate, User, UserPasswordUpdate
 
 router = APIRouter(prefix="/users/me", tags=["mypage"])
 
@@ -13,16 +15,21 @@ async def me(current_user: User = Depends(get_current_user)):
 
 
 @router.put("", response_model=User)
-async def update_me(data: UserUpdate, current_user: User = Depends(get_current_user)):
-    return await crud_users.update_user(current_user.uid, data)
+async def update_me(
+    data: SelfUserUpdate,
+    current_user: User = Depends(get_current_user),
+    db: Session = Depends(get_db),
+):
+    return await crud_users.update_user(db, current_user.id, data)
 
 
-@router.put("/password", response_model=User)
+@router.put("/password", response_model=bool)
 async def update_password(
     data: UserPasswordUpdate,
     current_user: User = Depends(get_current_user),
+    db: Session = Depends(get_db),
 ):
-    return await crud_users.upate_password(current_user.uid, data)
+    return await crud_users.upate_password(db, current_user.id, data)
 
 
 @router.delete("")
