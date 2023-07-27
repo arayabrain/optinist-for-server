0ce5a5f010420c734f0ebcd3c592f1dc1575d3a6
quanpython <quannd@reactplus.com>
2023-07-27 17:03:14 +0700

    add pagination transformer

--- studio/app/optinist/schemas/expdb/cell.py
+++ studio/app/optinist/schemas/expdb/cell.py
@@ -11,10 +11,12 @@ from studio.app.optinist.schemas.expdb.experiment import (
 
 class ExpDbCell(BaseModel):
     id: int
-    experiment_uid: int
     experiment_id: str = None
     fields: ExpDbExperimentFields = None
     cell_image_url: ImageInfo = None
     graph_urls: List[ImageInfo] = None
     created_at: datetime = None
     updated_at: datetime = None
+
+    class Config:
+        orm_mode = True
