8ffb9b6d8a6c140c0ce9c627cbf3c6dee6f23101
ReiHashimoto <42664619+ReiHashimoto@users.noreply.github.com>
2023-08-01 07:29:24 +0900

    add hooks to switch workflow with validation

--- frontend/src/store/slice/Workspace/WorkspaceSelector.ts
+++ frontend/src/store/slice/Workspace/WorkspaceSelector.ts
@@ -1,8 +1,25 @@
+import { IS_STANDALONE } from 'const/Mode'
 import { RootState } from 'store/store'
 
 export const selectWorkspace = (state: RootState) => state.workspace
-export const selectWorkspaceData = (state: RootState) => state.workspace.workspace
-export const selectWorkspaceListUserShare = (state: RootState) => state.workspace.listUserShare
+export const selectWorkspaceListUserShare = (state: RootState) =>
+  state.workspace.listUserShare
+export const selectWorkspaceData = (state: RootState) =>
+  state.workspace.workspace
+
+export const selectWorkspaceItem =
+  (workspaceId: number) => (state: RootState) =>
+    selectWorkspaceData(state).items.find((item) => item.id === workspaceId)
+
+export const selectWorkspaceItemExists =
+  (workspaceId?: number) => (state: RootState) =>
+    workspaceId && selectWorkspaceItem(workspaceId)(state) !== void 0
+
+export const selectIsCurrentUserOwnerWorkspaceItem =
+  (workspaceId: number, userId?: number) => (state: RootState) =>
+    IS_STANDALONE
+      ? true
+      : selectWorkspaceItem(workspaceId)(state)?.user.id === userId
 
 export const selectActiveTab = (state: RootState) =>
   state.workspace.currentWorkspace.selectedTab
