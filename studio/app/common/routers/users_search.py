9e526a4cd666f1e60e481d3ccf365f9c82a709d1
quanpython <quannd@reactplus.com>
2023-07-31 21:57:37 +0700

    add user search API

---
+++ studio/app/common/routers/users_search.py
@@ -0,0 +1,40 @@
+from typing import List
+
+from fastapi import APIRouter, Depends, Query
+from sqlmodel import Session, or_
+
+from studio.app.common.core.auth.auth_dependencies import get_current_user
+from studio.app.common.db.database import get_db
+from studio.app.common.models import User as UserModel
+from studio.app.common.schemas.users import UserInfo
+
+router = APIRouter(prefix="/users/search", tags=["users/search"])
+
+
+@router.get(
+    "/share_users",
+    response_model=List[UserInfo],
+    dependencies=[Depends(get_current_user)],
+    description="""
+- Get a list of users with whom content is shared.
+- Note: Maximum of 10 responses. (security considerations)
+""",
+)
+def search_share_users(
+    keyword: str = Query(description="partial match (user.name or user.email)"),
+    db: Session = Depends(get_db),
+):
+    MAX_RESPONSE_COUNT = 10
+    users = (
+        db.query(UserModel)
+        .filter(
+            or_(
+                UserModel.name.like("%{0}%".format(keyword)),
+                UserModel.email.like("%{0}%".format(keyword)),
+            )
+        )
+        .order_by(UserModel.id)
+        .limit(MAX_RESPONSE_COUNT)
+        .all()
+    )
+    return users
