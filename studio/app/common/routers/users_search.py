8d527d4c7ecbdddc3e1ab3474ea2810d2b81741a
quanpython <quannd@reactplus.com>
2023-08-01 11:04:06 +0700

    check active status and organization id

--- studio/app/common/routers/users_search.py
+++ studio/app/common/routers/users_search.py
@@ -6,7 +6,7 @@ from sqlmodel import Session, or_
 from studio.app.common.core.auth.auth_dependencies import get_current_user
 from studio.app.common.db.database import get_db
 from studio.app.common.models import User as UserModel
-from studio.app.common.schemas.users import UserInfo
+from studio.app.common.schemas.users import User, UserInfo
 
 router = APIRouter(prefix="/users/search", tags=["users/search"])
 
@@ -14,7 +14,6 @@ router = APIRouter(prefix="/users/search", tags=["users/search"])
 @router.get(
     "/share_users",
     response_model=List[UserInfo],
-    dependencies=[Depends(get_current_user)],
     description="""
 - Get a list of users with whom content is shared.
 - Note: Maximum of 10 responses. (security considerations)
@@ -23,10 +22,15 @@ router = APIRouter(prefix="/users/search", tags=["users/search"])
 def search_share_users(
     keyword: str = Query(description="partial match (user.name or user.email)"),
     db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user),
 ):
     MAX_RESPONSE_COUNT = 10
     users = (
         db.query(UserModel)
+        .filter(
+            UserModel.active.is_(True),
+            UserModel.organization_id == current_user.organization_id,
+        )
         .filter(
             or_(
                 UserModel.name.like("%{0}%".format(keyword)),
