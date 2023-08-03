d94e6baa650e1786f61a21c6293bd3add19a13bf
quanpython <quannd@reactplus.com>
2023-08-03 19:16:56 +0700

    add sort and filter for user list API

--- studio/app/common/schemas/users.py
+++ studio/app/common/schemas/users.py
@@ -2,9 +2,15 @@ from datetime import datetime
 from enum import Enum
 from typing import Optional
 
+from fastapi import Query
 from pydantic import BaseModel, EmailStr, Field
 
 password_regex = r"^(?=.*\d)(?=.*[!#$%&()*+,-./@_|])(?=.*[a-zA-Z]).{6,255}$"
+
+
+class UserSearchOptions(BaseModel):
+    email: Optional[str] = Field(Query(default=""))
+    name: Optional[str] = Field(Query(default=""))
 
 
 class UserRole(int, Enum):
