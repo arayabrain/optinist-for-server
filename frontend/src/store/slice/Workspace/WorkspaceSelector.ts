7a24e9c517160ac75e036d39dd2636c890464d89
ReiHashimoto <42664619+ReiHashimoto@users.noreply.github.com>
2023-08-07 12:45:01 +0900

    update workspace accessibility check

--- frontend/src/store/slice/Workspace/WorkspaceSelector.ts
+++ frontend/src/store/slice/Workspace/WorkspaceSelector.ts
@@ -1,4 +1,3 @@
-import { IS_STANDALONE } from 'const/Mode'
 import { RootState } from 'store/store'
 
 export const selectWorkspace = (state: RootState) => state.workspace
@@ -11,21 +10,14 @@ export const selectWorkspaceItem =
   (workspaceId: number) => (state: RootState) =>
     selectWorkspaceData(state).items.find((item) => item.id === workspaceId)
 
-export const selectWorkspaceItemExists =
-  (workspaceId?: number) => (state: RootState) =>
-    workspaceId && selectWorkspaceItem(workspaceId)(state) !== void 0
-
-export const selectIsCurrentUserOwnerWorkspaceItem =
-  (workspaceId: number, userId?: number) => (state: RootState) =>
-    IS_STANDALONE
-      ? true
-      : selectWorkspaceItem(workspaceId)(state)?.user.id === userId
-
 export const selectActiveTab = (state: RootState) =>
   state.workspace.currentWorkspace.selectedTab
 
 export const selectCurrentWorkspaceId = (state: RootState) =>
   state.workspace.currentWorkspace.workspaceId
+
+export const selectCurrentWorkspaceOwnerId = (state: RootState) =>
+  state.workspace.currentWorkspace.ownerId
 
 export const selectIsLoadingWorkspaceList = (state: RootState) =>
   state.workspace.loading
