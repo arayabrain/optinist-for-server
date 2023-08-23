1d23caf8142088360b188d50532ee40573eda9c1
ReiHashimoto <42664619+ReiHashimoto@users.noreply.github.com>
2023-08-23 10:43:39 +0900

    use exp id for batch execute params

--- studio/app/optinist/routers/expdb_batch.py
+++ studio/app/optinist/routers/expdb_batch.py
@@ -1,6 +1,6 @@
 import os
 
-from fastapi import APIRouter, BackgroundTasks
+from fastapi import APIRouter, HTTPException, Response
 
 from studio.app.common.core.utils.config_handler import ConfigReader
 from studio.app.common.core.utils.filepath_creater import (
@@ -9,6 +9,7 @@ from studio.app.common.core.utils.filepath_creater import (
 )
 from studio.app.common.core.utils.filepath_finder import find_param_filepath
 from studio.app.const import TC_SUFFIX, TS_SUFFIX
+from studio.app.dir_path import DIRPATH
 from studio.app.optinist.dataclass.expdb import ExpDbData
 from studio.app.optinist.wrappers.stat import (
     curvefit_tuning,
@@ -25,51 +26,60 @@ def get_default_params(name: str):
     return ConfigReader.read(filepath)
 
 
-def expdb_batch(dirpath: str, expdb: ExpDbData):
-    plot_dir = join_filepath([dirpath, "plots"])
-    create_directory(plot_dir, delete_dir=True)
+async def expdb_batch(exp_id: str):
+    try:
+        subject_id = exp_id.split("_")[0]
+        exp_dir = join_filepath([DIRPATH.EXPDB_DIR, subject_id, exp_id])
 
-    stat = stat_file_convert(
-        expdb=expdb,
-        output_dir=plot_dir,
-        params=get_default_params("stat_file_convert"),
-        export_plot=True,
-    )
+        if not (os.path.exists(exp_dir) and os.path.isdir(exp_dir)):
+            raise Exception(f"Data for experiment_id: {exp_id} does not exist")
 
-    oneway_anova(
-        stat=stat,
-        output_dir=plot_dir,
-        params=get_default_params("oneway_anova"),
-        export_plot=True,
-    )
+        expdb = ExpDbData(
+            paths=[
+                join_filepath([exp_dir, f"{exp_id}_{TS_SUFFIX}.mat"]),
+                join_filepath([exp_dir, f"{exp_id}_{TC_SUFFIX}.mat"]),
+            ]
+        )
 
-    curvefit_tuning(
-        stat=stat,
-        output_dir=plot_dir,
-        params=get_default_params("curvefit_tuning"),
-        export_plot=True,
-    )
+        plot_dir = join_filepath([exp_dir, "plots"])
+        create_directory(plot_dir, delete_dir=True)
 
-    vector_average(
-        stat=stat,
-        output_dir=plot_dir,
-        params=get_default_params("vector_average"),
-        export_plot=True,
-    )
+        stat = stat_file_convert(
+            expdb=expdb,
+            output_dir=plot_dir,
+            params=get_default_params("stat_file_convert"),
+            export_plot=True,
+        )
+
+        oneway_anova(
+            stat=stat,
+            output_dir=plot_dir,
+            params=get_default_params("oneway_anova"),
+            export_plot=True,
+        )
+
+        curvefit_tuning(
+            stat=stat,
+            output_dir=plot_dir,
+            params=get_default_params("curvefit_tuning"),
+            export_plot=True,
+        )
+
+        vector_average(
+            stat=stat,
+            output_dir=plot_dir,
+            params=get_default_params("vector_average"),
+            export_plot=True,
+        )
+        return {"status": "success"}
+    except Exception as e:
+        return {"status": "failed", "message": str(e)}
 
 
 @router.post("")
-async def run_expdb_batch(expdb_dirpath: str, background_tasks: BackgroundTasks):
-    assert os.path.exists(expdb_dirpath) and os.path.isdir(
-        expdb_dirpath
-    ), f"Directory {expdb_dirpath} does not exist"
-
-    exp_id = os.path.basename(expdb_dirpath)
-    paths = [
-        os.path.join(expdb_dirpath, f"{exp_id}_{TC_SUFFIX}.mat"),
-        os.path.join(expdb_dirpath, f"{exp_id}_{TS_SUFFIX}.mat"),
-    ]
-    expdb = ExpDbData(paths, None)
-
-    background_tasks.add_task(expdb_batch, expdb_dirpath, expdb)
-    return True
+async def run_expdb_batch(exp_id: str):
+    res = await expdb_batch(exp_id)
+    if res["status"] == "failed":
+        raise HTTPException(status_code=404, detail=res["message"])
+    else:
+        return Response(status_code=200)
