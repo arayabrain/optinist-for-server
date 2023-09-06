c68e1e9259c5129cb6498341ba1018939c31c5cd
ReiHashimoto <42664619+ReiHashimoto@users.noreply.github.com>
2023-09-06 21:47:56 +0900

    improve cell bulk operations

--- studio/app/optinist/core/expdb/crud_cells.py
+++ studio/app/optinist/core/expdb/crud_cells.py
@@ -1,35 +1,29 @@
-from fastapi import HTTPException
-from sqlmodel import Session
+from sqlmodel import Session, insert
 
 from studio.app.optinist.models import Cell as CellModel
 from studio.app.optinist.models import Experiment as ExperimentModel
 
 
 def bulk_insert_cells(db: Session, experiment_uid: int, cells: int):
-    try:
-        experiment = db.query(ExperimentModel).get(experiment_uid)
-        assert experiment is not None, "Experiment not found"
+    experiment = db.query(ExperimentModel).get(experiment_uid)
+    assert experiment is not None, "Experiment not found"
 
-        db.query(CellModel).filter(CellModel.experiment_uid == experiment_uid).delete()
+    db.query(CellModel).filter(CellModel.experiment_uid == experiment_uid).delete()
 
-        for cell in range(cells):
-            db.add(CellModel(cell_number=cell, experiment_uid=experiment_uid))
+    db.execute(
+        insert(CellModel),
+        [
+            {"cell_number": cell_number, "experiment_uid": experiment_uid}
+            for cell_number in range(cells)
+        ],
+    )
 
-        db.commit()
-
-        return True
-    except AssertionError as e:
-        raise HTTPException(status_code=404, detail=str(e))
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
+    db.commit()
+    return True
 
 
 def bulk_delete_cells(db: Session, experiment_uid: int):
-    try:
-        db.query(CellModel).filter(CellModel.experiment_uid == experiment_uid).delete()
+    db.query(CellModel).filter(CellModel.experiment_uid == experiment_uid).delete()
 
-        db.commit()
-
-        return True
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
+    db.commit()
+    return True
