dcef7abb9e93431655c570cd5a31178d8923a1c5
ReiHashimoto <42664619+ReiHashimoto@users.noreply.github.com>
2023-09-07 21:05:59 +0900

    add feature to import/export workflow

--- studio/__main_unit__.py
+++ studio/__main_unit__.py
@@ -23,6 +23,7 @@ from studio.app.common.routers import (
     users_admin,
     users_me,
     users_search,
+    workflow,
     workspace,
 )
 from studio.app.dir_path import DIRPATH
@@ -43,6 +44,7 @@ app.include_router(run.router, dependencies=[Depends(get_current_user)])
 app.include_router(users_admin.router, dependencies=[Depends(get_admin_user)])
 app.include_router(users_me.router, dependencies=[Depends(get_current_user)])
 app.include_router(users_search.router, dependencies=[Depends(get_current_user)])
+app.include_router(workflow.router, dependencies=[Depends(get_current_user)])
 app.include_router(workspace.router, dependencies=[Depends(get_current_user)])
 
 # optinist routers
