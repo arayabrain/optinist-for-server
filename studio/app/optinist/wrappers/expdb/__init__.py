a9a908397f406c573362c78ce138726f7b1ea233
ReiHashimoto <42664619+ReiHashimoto@users.noreply.github.com>
2023-09-13 13:14:31 +0900

    rename expdb wrappers

--- studio/app/optinist/wrappers/expdb/__init__.py
+++ studio/app/optinist/wrappers/expdb/__init__.py
@@ -6,25 +6,29 @@ from studio.app.optinist.wrappers.expdb.vector_average import vector_average
 
 expdb_wrapper_dict = {
     "expdb": {
-        "analyze_stats": {
-            "function": analyze_stats,
-            "conda_name": "expdb",
+        "analysis_preset": {
+            "analyze_stats": {
+                "function": analyze_stats,
+                "conda_name": "expdb",
+            },
         },
-        "stat_file_convert": {
-            "function": stat_file_convert,
-            "conda_name": "expdb",
-        },
-        "anova1_mult": {
-            "function": anova1_mult,
-            "conda_name": "expdb",
-        },
-        "vector_average": {
-            "function": vector_average,
-            "conda_name": "expdb",
-        },
-        "curvefit_tuning": {
-            "function": curvefit_tuning,
-            "conda_name": "expdb",
+        "preset_components": {
+            "stat_file_convert": {
+                "function": stat_file_convert,
+                "conda_name": "expdb",
+            },
+            "anova1_mult": {
+                "function": anova1_mult,
+                "conda_name": "expdb",
+            },
+            "vector_average": {
+                "function": vector_average,
+                "conda_name": "expdb",
+            },
+            "curvefit_tuning": {
+                "function": curvefit_tuning,
+                "conda_name": "expdb",
+            },
         },
     }
 }
