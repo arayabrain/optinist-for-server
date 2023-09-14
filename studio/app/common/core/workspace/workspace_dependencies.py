4f1651326f79b516dceb0b008b3736e1c230acd1
ReiHashimoto <42664619+ReiHashimoto@users.noreply.github.com>
2023-09-14 12:16:41 +0900

    seprate owner and shared scopes

--- studio/app/common/core/workspace/workspace_dependencies.py
+++ studio/app/common/core/workspace/workspace_dependencies.py
@@ -9,6 +9,27 @@ from studio.app.common.db.database import get_db
 from studio.app.common.schemas.users import User
 
 
+async def is_workspace_owner(
+    workspace_id: Union[int, str],
+    current_user: User = Depends(get_current_user),
+    db: Session = Depends(get_db),
+):
+    workspace = (
+        db.query(common_model.Workspace)
+        .filter(
+            common_model.Workspace.id == workspace_id,
+            common_model.Workspace.deleted.is_(False),
+            common_model.Workspace.user_id == current_user.id,
+        )
+        .first()
+    )
+
+    if workspace is None:
+        raise HTTPException(status_code=403, detail="Operation is not available")
+    else:
+        return True
+
+
 async def is_workspace_available(
     workspace_id: Union[int, str],
     current_user: User = Depends(get_current_user),
