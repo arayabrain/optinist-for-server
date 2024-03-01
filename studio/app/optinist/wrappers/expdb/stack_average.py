2e1d5519dac931fa436571e8f7eb16c27b73464d
ReiHashimoto <42664619+ReiHashimoto@users.noreply.github.com>
2024-03-01 15:57:10 +0900

    fix stack shape assumption

--- studio/app/optinist/wrappers/expdb/stack_average.py
+++ studio/app/optinist/wrappers/expdb/stack_average.py
@@ -16,32 +16,29 @@ def stack_average(stack: np.ndarray, period, runs=None):
     Returns
     -------
     ndarray
-        average stack, double float
+        average stack with shape(y, x, (z)), double float
     """
-    # stack: (ch, frames, y, x) or (ch, frames, z, y, x)
+    # stack: (y, x, t) or (y, x, z, t)
     dim = stack.shape
     d = len(dim)
-    frames = dim[1]
+    assert d in [3, 4], "stack must be 2d, 3d"
+    frames = dim[-1]
 
     if runs is None:
         runs = list(range(int(frames / period)))
 
-    if d == 4:
-        avg = np.zeros((1, period, dim[-2], dim[-1]))
-    elif d == 5:
-        avg = np.zeros((1, period, dim[-3], dim[-2], dim[-1]))
-    else:
-        return None
+    if d == 3:
+        avg = np.zeros((dim[0], dim[1], period))  # (y, x, t)
+    elif d == 4:
+        avg = np.zeros((dim[0], dim[1], dim[2], period))  # (y, x, z, t)
 
     for r in runs:
         start = r * period
         end = (r + 1) * period
-        if d == 4:
-            avg += stack[:, start:end, :, :].astype(float)
-        elif d == 5:
-            avg += stack[:, start:end, :, :, :].astype(float)
-        else:
-            return None
+        if d == 3:
+            avg += stack[:, :, start:end].astype(float)
+        elif d == 4:
+            avg += stack[:, :, :, start:end].astype(float)
 
     avg /= len(runs)
 
