f6e798efb6e81bd02d2bbe67f1db1b6d1b597216
ReiHashimoto <42664619+ReiHashimoto@users.noreply.github.com>
2024-03-04 16:40:51 +0900

    update stack phase correction

--- studio/app/optinist/wrappers/expdb/stack_phase_correct.py
+++ studio/app/optinist/wrappers/expdb/stack_phase_correct.py
@@ -23,45 +23,48 @@ def stack_phase_correct(stack, fov, first_dim=1):
     int
         phase shift
     """
-    from scipy.ndimage import shift
-
-    # stack: (ch, frames, (z,) y, x)
-    stack_dim = stack.shape
-    # fov: ((z,) y, x)
-    fov_dim = fov.shape
-    assert len(fov_dim) > 3, "fov must be 2d or 3d"
+    stack_dim = stack.shape  # (y, x, t) or (y, x, z, t)
+    fov_dim = fov.shape  # (y, x) or (y, x, z)
+    assert len(fov_dim) in [2, 3], "fov must be 2d or 3d"
     assert first_dim in [1, 2], "first_dim must be 1 or 2"
 
     if first_dim == 1:
         # separate odd lines and even lines
-        fov = fov.reshape([fov_dim[0], 2, int(fov_dim[1] / 2)] + list(fov_dim[3:]))
+        reshaped_fov = fov.reshape(
+            [int(fov_dim[1] / 2), 2, fov_dim[0], int(np.prod(fov_dim[2:]))]
+        )
 
         # estimate phase mismatch
-        outs = stack_register_nD(fov[:, 0, :, :], fov[:, 1, :, :])
-        dx = -outs[2]
+        outs, out_stack = stack_register_nD(
+            reshaped_fov[:, 0, :, :], reshaped_fov[:, 1, :, :]
+        )
+        dx = -outs[1][0]
 
         # align phase of stack
-        stack = stack.reshape(
-            [stack_dim[0], 2, int(stack_dim[1] / 2)] + list(stack_dim[3:])
+        result_stack = stack.reshape(
+            [int(stack_dim[1] / 2), 2, stack_dim[0], int(np.prod(stack_dim[2:]))]
         )
-        for i in range(np.prod(stack_dim[3:])):
-            stack[:, 1, :, i] = shift(stack[:, 1, :, i], [dx, 0])
-        stack = stack.reshape(stack_dim)
+        for i in range(np.prod(stack_dim[2:])):
+            result_stack[:, 1, :, i] = np.roll(result_stack[:, 1, :, i], dx, axis=1)
 
     elif first_dim == 2:
         # separate odd lines and even lines
-        fov = fov.reshape([2, int(fov_dim[0] / 2), fov_dim[1]] + list(fov_dim[3:]))
+        reshaped_fov = fov.reshape(
+            [2, fov_dim[1], int(fov_dim[0] / 2), int(np.prod(fov_dim[2:]))]
+        )
 
         # estimate phase mismatch
-        outs = stack_register_nD(fov[0, :, :, :], fov[1, :, :, :])
-        dx = -outs[3]
+        outs, outstack = stack_register_nD(
+            np.squeeze(reshaped_fov[0, :, :, :]), np.squeeze(reshaped_fov[1, :, :, :])
+        )
+        dx = -outs[1][1]
 
         # align phase of stack
-        stack = stack.reshape(
-            [2, int(stack_dim[0] / 2), stack_dim[1]] + list(stack_dim[3:])
+        result_stack = stack.reshape(
+            [2, int(stack_dim[0] / 2), stack_dim[1], int(np.prod(stack_dim[2:]))]
         )
         for i in range(np.prod(stack_dim[3:])):
-            stack[1, :, :, i] = shift(stack[1, :, :, i], [0, 0, dx])
-        stack = stack.reshape(stack_dim)
+            result_stack[1, :, :, i] = np.roll(result_stack[1, :, :, i], [0, 0, dx])
 
-    return stack, dx
+    result_stack = result_stack.reshape(stack_dim)
+    return result_stack, dx
