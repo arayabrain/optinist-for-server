5ea9c595862d2d0bea8faeb65d4d2beb726db142
itutu-tienday <tienday@itutu.local>
2024-04-05 13:59:49 +0900

    add logging for wrapper (caiman)

--- studio/app/optinist/wrappers/caiman/cnmf.py
+++ studio/app/optinist/wrappers/caiman/cnmf.py
@@ -5,6 +5,7 @@ import shutil
 import numpy as np
 import scipy
 
+from studio.app.common.core.logger import AppLogger
 from studio.app.common.core.utils.filepath_creater import join_filepath
 from studio.app.common.dataclass import ImageData
 from studio.app.const import TC_SUFFIX, TS_SUFFIX
@@ -12,6 +13,8 @@ from studio.app.dir_path import DIRPATH
 from studio.app.optinist.core.nwb.nwb import NWBDATASET
 from studio.app.optinist.dataclass import EditRoiData, FluoData, IscellData, RoiData
 from studio.app.optinist.dataclass.expdb import ExpDbData
+
+logger = AppLogger.get_logger()
 
 
 def get_roi(A, roi_thr, thr_method, swap_dim, dims):
@@ -148,7 +151,7 @@ def caiman_cnmf(
     from caiman.source_extraction.cnmf.params import CNMFParams
 
     function_id = output_dir.split("/")[-1]
-    print("start caiman_cnmf:", function_id)
+    logger.info(f"start caiman_cnmf: {function_id}")
 
     # NOTE: evaluate_components requires cnn_model files in caiman_data directory.
     caiman_data_dir = os.path.join(os.path.expanduser("~"), "caiman_data")
