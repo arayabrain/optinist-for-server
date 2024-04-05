93f7387051826facd07760e880621cbcdb702711
tienday <develop@itutu.local>
2024-04-05 11:00:45 +0900

    Rename existing logger class

--- studio/app/common/core/snakemake/smk_status_logger.py
+++ studio/app/common/core/snakemake/smk_status_logger.py
@@ -10,43 +10,48 @@ from studio.app.common.core.utils.filepath_creater import (
 from studio.app.dir_path import DIRPATH
 
 
-def get_logger(workspace_id: str, unique_id: str) -> logging.Logger:
-    output_dirpath = join_filepath(
-        [
-            DIRPATH.OUTPUT_DIR,
-            workspace_id,
-            unique_id,
-        ]
-    )
-    create_directory(output_dirpath)
+class SmkStatusLogger:
+    """
+    TODO: Since the Snakemake library automatically creates thread for workflows and
+    shares the same loggers in the library, all workflows running at the same time
+    will use same log data.
+    """
 
-    filepath = f"{output_dirpath}/error.log"
-    if os.path.exists(filepath):
-        try:
-            os.remove(filepath)
-        except Exception as e:
-            print("[Exception][Logger]", e)
+    @staticmethod
+    def get_logger(workspace_id: str, unique_id: str) -> logging.Logger:
+        output_dirpath = join_filepath(
+            [
+                DIRPATH.OUTPUT_DIR,
+                workspace_id,
+                unique_id,
+            ]
+        )
+        create_directory(output_dirpath)
 
-    logger = logging.getLogger(unique_id)
+        filepath = f"{output_dirpath}/error.log"
+        if os.path.exists(filepath):
+            try:
+                os.remove(filepath)
+            except Exception as e:
+                print("[Exception][Logger]", e)
 
-    # FileHandlerの設定
-    fh = logging.FileHandler(filepath)
-    fh.setLevel(logging.ERROR)
-    fmt = logging.Formatter("%(asctime)s : %(levelname)s - %(filename)s - %(message)s")
-    fh.setFormatter(fmt)
-    logger.addHandler(fh)
-    fh.close()
-    return logger
+        logger = logging.getLogger(unique_id)
 
+        # FileHandlerの設定
+        fh = logging.FileHandler(filepath)
+        fh.setLevel(logging.ERROR)
+        fmt = logging.Formatter(
+            "%(asctime)s : %(levelname)s - %(filename)s - %(message)s"
+        )
+        fh.setFormatter(fmt)
+        logger.addHandler(fh)
+        fh.close()
+        return logger
 
-class Logger:
-    # TODO: Since the Snakemake library automatically creates thread for workflows and
-    # shares the same loggers in the library, all workflows running at the same time
-    # will use same log data.
     def __init__(self, workspace_id, unique_id):
         self.workspace_id = workspace_id
         self.unique_id = unique_id
-        self.logger: logging.Logger = get_logger(workspace_id, unique_id)
+        self.logger: logging.Logger = __class__.get_logger(workspace_id, unique_id)
 
     def smk_logger(self, msg: Dict[str, str] = None):
         """
