be6a794294b577b1b1fb1b6ec3e8248b30ab376a
itutu-tienday <>
2024-12-17 18:48:12 +0900

    fix lint error

--- studio/app/optinist/wrappers/__init__.py
+++ studio/app/optinist/wrappers/__init__.py
@@ -1,9 +1,8 @@
-from studio.app.optinist.wrappers.expdb import expdb_wrapper_dict
-
 # from studio.app.optinist.wrappers.caiman import caiman_wrapper_dict
+# from studio.app.optinist.wrappers.suite2p import suite2p_wrapper_dict
+from studio.app.optinist.wrappers.expdb import expdb_wrapper_dict
 from studio.app.optinist.wrappers.lccd import lccd_wrapper_dict
 from studio.app.optinist.wrappers.optinist import optinist_wrapper_dict
-# from studio.app.optinist.wrappers.suite2p import suite2p_wrapper_dict
 
 wrapper_dict = {}
 # wrapper_dict.update(**caiman_wrapper_dict)
