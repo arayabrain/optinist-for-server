d6b8f506dd9769467b3a2e91d484b8000e4d1025
tienday <develop@itutu.local>
2025-01-16 21:07:18 +0900

    adjust pytest for test_algolist

--- studio/tests/app/common/routers/test_algolist.py
+++ studio/tests/app/common/routers/test_algolist.py
@@ -11,31 +11,49 @@ def test_run(client):
 
     assert response.status_code == 200
     assert isinstance(output, dict)
-    assert ALGO_TOP_GROUP in output
-    assert "children" in output[ALGO_TOP_GROUP]
-    assert "analysis_preset" in output[ALGO_TOP_GROUP]["children"]
-    assert "children" in output[ALGO_TOP_GROUP]["children"]["analysis_preset"]
-    assert "analyze_stats" in output[ALGO_TOP_GROUP]["children"]["analysis_preset"]["children"]
 
-    assert (
-        "args" in output[ALGO_TOP_GROUP]["children"]["analysis_preset"]["children"]["analyze_stats"]
-    )
-    assert (
-        "path" in output[ALGO_TOP_GROUP]["children"]["analysis_preset"]["children"]["analyze_stats"]
-    )
-    assert "preset_components" in output[ALGO_TOP_GROUP]["children"]
+    assert ALGO_TOP_GROUP in output
+    output_lv1 = output[ALGO_TOP_GROUP]
+
+    assert "children" in output_lv1
+    output_lv2 = output_lv1["children"]
+
+    assert "analysis_preset" in output_lv2
+    output_lv3 = output_lv2["analysis_preset"]
+
+    assert "children" in output_lv3
+    output_lv4 = output_lv3["children"]
+
+    assert "analyze_stats" in output_lv4
+    output_lv5 = output_lv4["analyze_stats"]
+
+    assert "args" in output_lv5
+    assert "path" in output_lv5
+
+    assert "preset_components" in output_lv2
 
 
 def test_NestDictGetter():
     output = NestDictGetter.get_nest_dict(wrapper_dict, "")
 
     assert isinstance(output, dict)
+
     assert ALGO_TOP_GROUP in output
-    assert "children" in output[ALGO_TOP_GROUP]
-    assert "analysis_preset" in output[ALGO_TOP_GROUP]["children"]
-    assert "children" in output[ALGO_TOP_GROUP]["children"]["analysis_preset"]
-    assert "analyze_stats" in output[ALGO_TOP_GROUP]["children"]["analysis_preset"]["children"]
+    output_lv1 = output[ALGO_TOP_GROUP]
+
+    assert "children" in output_lv1
+    output_lv2 = output_lv1["children"]
+
+    assert "analysis_preset" in output_lv2
+    output_lv3 = output_lv2["analysis_preset"]
+
+    assert "children" in output_lv3
+    output_lv4 = output_lv3["children"]
+
+    assert "analyze_stats" in output_lv4
+    output_lv5 = output_lv4["analyze_stats"]
 
     assert isinstance(
-        output[ALGO_TOP_GROUP]["children"]["analysis_preset"]["children"]["analyze_stats"], Algo
+        output_lv5,
+        Algo,
     )
