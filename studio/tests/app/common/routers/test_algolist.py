8236443ddabc69ccec35104a6aa8b859c63ac435
tienday <develop@itutu.local>
2025-01-16 20:56:13 +0900

    adjust pytest for test_algolist

--- studio/tests/app/common/routers/test_algolist.py
+++ studio/tests/app/common/routers/test_algolist.py
@@ -2,6 +2,8 @@ from studio.app.common.routers.algolist import NestDictGetter
 from studio.app.common.schemas.algolist import Algo
 from studio.app.wrappers import wrapper_dict
 
+ALGO_TOP_GROUP = "expdb"
+
 
 def test_run(client):
     response = client.get("/algolist")
@@ -9,21 +11,31 @@ def test_run(client):
 
     assert response.status_code == 200
     assert isinstance(output, dict)
-    assert "analysis_preset" in output
-    assert "children" in output["analysis_preset"]
-    assert "analyze_stats" in output["analysis_preset"]["children"]
+    assert ALGO_TOP_GROUP in output
+    assert "children" in output[ALGO_TOP_GROUP]
+    assert "analysis_preset" in output[ALGO_TOP_GROUP]["children"]
+    assert "children" in output[ALGO_TOP_GROUP]["children"]["analysis_preset"]
+    assert "analyze_stats" in output[ALGO_TOP_GROUP]["children"]["analysis_preset"]["children"]
 
-    assert "args" in output["analysis_preset"]["children"]["analyze_stats"]
-    assert "path" in output["analysis_preset"]["children"]["analyze_stats"]
-    assert "preset_components" in output
+    assert (
+        "args" in output[ALGO_TOP_GROUP]["children"]["analysis_preset"]["children"]["analyze_stats"]
+    )
+    assert (
+        "path" in output[ALGO_TOP_GROUP]["children"]["analysis_preset"]["children"]["analyze_stats"]
+    )
+    assert "preset_components" in output[ALGO_TOP_GROUP]["children"]
 
 
 def test_NestDictGetter():
     output = NestDictGetter.get_nest_dict(wrapper_dict, "")
 
     assert isinstance(output, dict)
-    assert "analysis_preset" in output
-    assert "children" in output["analysis_preset"]
-    assert "analyze_stats" in output["analysis_preset"]["children"]
+    assert ALGO_TOP_GROUP in output
+    assert "children" in output[ALGO_TOP_GROUP]
+    assert "analysis_preset" in output[ALGO_TOP_GROUP]["children"]
+    assert "children" in output[ALGO_TOP_GROUP]["children"]["analysis_preset"]
+    assert "analyze_stats" in output[ALGO_TOP_GROUP]["children"]["analysis_preset"]["children"]
 
-    assert isinstance(output["analysis_preset"]["children"]["analyze_stats"], Algo)
+    assert isinstance(
+        output[ALGO_TOP_GROUP]["children"]["analysis_preset"]["children"]["analyze_stats"], Algo
+    )
