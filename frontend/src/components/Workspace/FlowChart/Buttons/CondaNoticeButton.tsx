1240cb65521461494f3ad8190cb0fe4c79727995
itutu-tienday <>
2025-01-29 15:14:57 +0900

    add run create-conda-env workflow process (WIP)

--- frontend/src/components/Workspace/FlowChart/Buttons/CondaNoticeButton.tsx
+++ frontend/src/components/Workspace/FlowChart/Buttons/CondaNoticeButton.tsx
@@ -1,9 +1,20 @@
 import { memo, useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+
+import { enqueueSnackbar } from "notistack"
 
 import WarningIcon from "@mui/icons-material/Warning"
 import IconButton from "@mui/material/IconButton"
 
 import { ConfirmDialog } from "components/common/ConfirmDialog"
+import { getExperiments } from "store/slice/Experiments/ExperimentsActions"
+import { reset } from "store/slice/VisualizeItem/VisualizeItemSlice"
+import {
+  importSampleData,
+  reproduceWorkflow,
+} from "store/slice/Workflow/WorkflowActions"
+import { selectCurrentWorkspaceId } from "store/slice/Workspace/WorkspaceSelector"
+import { AppDispatch } from "store/store"
 
 interface CondaNoticeButtonProps {
   condaName: string
@@ -16,9 +27,41 @@ export const CondaNoticeButton = memo(function CondaNoticeButton({
   const openDialog = () => {
     setOpen(true)
   }
-  const handleOk = () => {
-    // TODO: Requires implementation
+
+  const dispatch = useDispatch<AppDispatch>()
+  const workspaceId = useSelector(selectCurrentWorkspaceId) || -1
+
+  const handleOk = async (condaName: string) => {
+    // Import create-conda-env workflow
+    await dispatch(importSampleData({ workspaceId }))
+      .unwrap()
+      .then(() => {
+        // *Here, success snackbar display is off.
+        dispatch(reset())
+        dispatch(getExperiments())
+      })
+      .catch(() => {
+        enqueueSnackbar("Failed to import import", { variant: "error" })
+        return // break on error
+      })
+
+    // Reproduce create-conda-env workflow
+    const uid = `conda_${condaName}`
+    await dispatch(reproduceWorkflow({ workspaceId, uid }))
+      .unwrap()
+      .then(() => {
+        // *Here, success snackbar display is off.
+        dispatch(reset())
+      })
+      .catch(() => {
+        enqueueSnackbar("Failed to reproduce", { variant: "error" })
+        return // break on error
+      })
+
+    // TODO: RUN reproduced workflow.
+    // (WIP)
   }
+
   return (
     <>
       <IconButton
@@ -31,7 +74,7 @@ export const CondaNoticeButton = memo(function CondaNoticeButton({
       <ConfirmDialog
         open={open}
         setOpen={setOpen}
-        onConfirm={handleOk}
+        onConfirm={() => handleOk(condaName)}
         title="Conda Environment Verification"
         content={
           <>
