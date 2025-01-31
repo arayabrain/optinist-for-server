58ca03b452df4ab09ee27e8898eb6bca8d8c0480
itutu-tienday <>
2025-01-31 19:19:48 +0900

    fix linter

--- studio/app/optinist/wrappers/__init__.py
+++ studio/app/optinist/wrappers/__init__.py
@@ -1,8 +1,8 @@
 from studio.app.optinist.wrappers.caiman import caiman_wrapper_dict
 from studio.app.optinist.wrappers.lccd import lccd_wrapper_dict
+from studio.app.optinist.wrappers.maintenance import maintenance_wrapper_dict
 from studio.app.optinist.wrappers.optinist import optinist_wrapper_dict
 from studio.app.optinist.wrappers.suite2p import suite2p_wrapper_dict
-from studio.app.optinist.wrappers.maintenance import maintenance_wrapper_dict
 
 wrapper_dict = {}
 wrapper_dict.update(**caiman_wrapper_dict)
