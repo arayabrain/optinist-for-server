3ecd916523a210f248aed3b71b52bcbcf4756582
tsuchiyama-araya <tsuchiyama_yutaka@araya.org>
2025-02-03 17:15:53 +0900

    roi_fluo_from_hdf5　改修

--- studio/app/optinist/wrappers/optinist/visualize_utils/roi_fluo_from_hdf5.py
+++ studio/app/optinist/wrappers/optinist/visualize_utils/roi_fluo_from_hdf5.py
@@ -34,36 +34,6 @@ def roi_fluo_from_hdf5(
 
     # Initialize outputs and mappings
     iscell_data = iscell.data if iscell else None
-    aligned_fluo = fluo.data
-    roi_to_fluo_map = {}
-
-    # Process ROI and fluorescence mapping
-    if iscell_data is not None:
-        iscell_data = iscell_data.reshape(
-            cell_img.data.shape[1], cell_img.data.shape[2]
-        )
-        for roi_idx in range(cell_img.data.shape[0]):
-            overlap = np.sum(
-                cell_img.data[roi_idx]
-                * iscell_data[: cell_img.data.shape[1], : cell_img.data.shape[2]],
-                axis=(0, 1),
-            )
-            fluo_idx = np.argmax(overlap)
-            roi_to_fluo_map[roi_idx] = fluo_idx
-
-        # Align fluorescence data using mapping
-        aligned_fluo = np.zeros_like(fluo.data)
-        for roi_idx, fluo_idx in roi_to_fluo_map.items():
-            aligned_fluo[roi_idx] = fluo.data[fluo_idx]
-
-    # Generate ROI masks and associated data
-    roi_masks = []
-    for i in range(cell_img.data.shape[0]):
-        mask = {
-            "roi_idx": i,
-            "mask": cell_img.data[i],
-        }
-        roi_masks.append(mask)
 
     # Enrich output with metadata and processed data
     output = {
@@ -74,32 +44,19 @@ def roi_fluo_from_hdf5(
             file_name="all_roi",
         ),
         "non_cell_roi": RoiData(
-            (
-                np.nanmax(cell_img.data[iscell_data == 0], axis=0)
-                if iscell_data is not None
-                else None
-            ),
+            (np.nanmax(cell_img.data[iscell_data == 0], axis=0)),
             output_dir=output_dir,
             file_name="noncell_roi",
         ),
         "cell_roi": RoiData(
-            (
-                np.nanmax(cell_img.data[iscell_data != 0], axis=0)
-                if iscell_data is not None
-                else None
-            ),
+            (np.nanmax(cell_img.data[iscell_data != 0], axis=0)),
             output_dir=output_dir,
             file_name="cell_roi",
         ),
         "fluorescence": FluoData(
-            np.transpose(aligned_fluo),
+            np.transpose(fluo.data),
             file_name="fluorescence",
         ),
-        "roi_masks": roi_masks,
-        "metadata": {
-            "roi_to_fluo_map": roi_to_fluo_map,
-            "params": params,
-        },
     }
 
     logger.info("Completed ROI and fluorescence data processing")
