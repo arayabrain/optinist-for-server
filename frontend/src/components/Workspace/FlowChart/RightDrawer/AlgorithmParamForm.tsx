4bf0d208c79071a8cdb79c3c4311b1b32e8a4813
tsuchiyama-araya <tsuchiyama_yutaka@araya.org>
2025-02-28 11:32:28 +0900

    特定できないリンクをMapping処理を実装

--- frontend/src/components/Workspace/FlowChart/RightDrawer/AlgorithmParamForm.tsx
+++ frontend/src/components/Workspace/FlowChart/RightDrawer/AlgorithmParamForm.tsx
@@ -29,7 +29,27 @@ export const AlgorithmParamForm = memo(function AlgorithmParamForm() {
     selectAlgorithmParamsKeyList(nodeId),
     arrayEqualityFn,
   )
-  const formattedAlgoName = algoName.toLowerCase().replace(/_/g, "-")
+  const algoNameMapping: { [key: string]: string } = {
+    eta: "eta-event-triggered-average",
+    cca: "cca-canonical-correlation-analysis",
+    dpca: "dpca-demixed-principal-component-analysis",
+    dca: "dca-dynamical-component-analysis",
+    tsne: "tsne-t-distributed-stochastic-neighbor-embedding",
+    glm: "glm-generalized-linear-model",
+    lda: "lda-linear-discriminant-analysis",
+    svm: "svm-support-vector-machine",
+    granger: "granger-granger-causality-test",
+    "lccd-detect": "lccd-cell-detection",
+    "microscope-to-img": "microscope-to-image",
+  }
+
+  let formattedAlgoName = algoName.toLowerCase().replace(/_/g, "-")
+
+  // Check if the formatted name exists in the mapping, otherwise keep it as is
+  formattedAlgoName =
+    algoNameMapping[formattedAlgoName as keyof typeof algoNameMapping] ||
+    formattedAlgoName
+
   const parameterUrl = `https://optinist.readthedocs.io/en/latest/specifications/algorithm_nodes.html#${formattedAlgoName}`
   useEffect(() => {
     if (!algoParamIsLoaded) {
