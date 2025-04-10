52b9628c38c1bb455bc0a231e9980832ade73912
itutu-tienday <>
2025-04-10 11:25:06 +0900

    Ajusted cron expdb_batch script - Manipulating env variable - Lightweight processing (pre-searching flag files)

--- cron/run_expdb_batch_script.sh
+++ cron/run_expdb_batch_script.sh
@@ -1,8 +1,24 @@
 #!/bin/sh
 
 . ~/.bashrc
-
 cd $(cd $(dirname $0); pwd)/../
 
+# Set enviroments
+if [ -z "${OPTINIST_DIR}" ] && [ -d "/app" ]; then
+ export OPTINIST_DIR="/app/studio_data"
+fi
+
+# Check expdb_batch .proc files
+DATASET_DIR="experiments_datasets"
+PROC_FILE_PATTERN="*.proc"
+proc_files=$(find "$DATASET_DIR" -maxdepth 2 -name "$PROC_FILE_PATTERN" -type f)
+if [ -z "$proc_files" ]; then
+  echo "No proc files found."
+  exit
+else
+  echo "Proc files found. [$proc_files]"
+fi
+
+# Run expdb_batch
 conda activate expdb_batch && \
   python run_expdb_batch.py -o 1 -p 5
