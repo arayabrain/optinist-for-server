5f69346637f309f90381e4ffff6da2bc6b2ca1a4
itutu-tienday <>
2025-10-21 09:49:21 +0900

    Analysis Batch GUI run) Adjusted WORKSPACE_TYPE (added EXPDB_BATCH type)

--- frontend/src/const/Workspace.ts
+++ frontend/src/const/Workspace.ts
@@ -2,12 +2,14 @@ export enum WORKSPACE_TYPE {
   DEFAULT = 0,
   NORMAL = 1,
   BATCH = 2,
+  EXPDB_BATCH = 10,
 }
 
 export const WORKSPACE_TYPE_LABEL: Record<WORKSPACE_TYPE, string> = {
   [WORKSPACE_TYPE.DEFAULT]: "Normal", // Same as NORMAL
   [WORKSPACE_TYPE.NORMAL]: "Normal",
   [WORKSPACE_TYPE.BATCH]: "Batch",
+  [WORKSPACE_TYPE.EXPDB_BATCH]: "Analysis Batch",
 }
 
 export const WORKSPACE_TYPE_OPTIONS = [
@@ -15,8 +17,14 @@ export const WORKSPACE_TYPE_OPTIONS = [
     value: WORKSPACE_TYPE.NORMAL,
     label: WORKSPACE_TYPE_LABEL[WORKSPACE_TYPE.NORMAL],
   },
+  /* NOTE: Currently (v2.5) unused in optinist-for-server
   {
     value: WORKSPACE_TYPE.BATCH,
     label: WORKSPACE_TYPE_LABEL[WORKSPACE_TYPE.BATCH],
+  },
+  */
+  {
+    value: WORKSPACE_TYPE.EXPDB_BATCH,
+    label: WORKSPACE_TYPE_LABEL[WORKSPACE_TYPE.EXPDB_BATCH],
   },
 ]
