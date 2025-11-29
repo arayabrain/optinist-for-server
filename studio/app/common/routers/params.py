02a69cf362b96c156d28226f08d6de51d631bb98
itutu-tienday <>
2025-11-29 09:47:15 +0900

    Added read_default_params function to workflow_params

--- studio/app/common/routers/params.py
+++ studio/app/common/routers/params.py
@@ -2,8 +2,7 @@ from typing import Any, Dict
 
 from fastapi import APIRouter
 
-from studio.app.common.core.utils.config_handler import ConfigReader
-from studio.app.common.core.utils.filepath_finder import find_param_filepath
+from studio.app.common.core.workflow.workflow_params import read_default_params
 from studio.app.common.schemas.params import SnakemakeParams
 
 router = APIRouter(tags=["params"])
@@ -11,12 +10,9 @@ router = APIRouter(tags=["params"])
 
 @router.get("/params/{name}", response_model=Dict[str, Any])
 async def get_params(name: str):
-    filepath = find_param_filepath(name)
-    config = ConfigReader.read(filepath)
-    return config
+    return read_default_params(name)
 
 
 @router.get("/snakemake", response_model=SnakemakeParams)
 async def get_snakemake_params():
-    filepath = find_param_filepath("snakemake")
-    return ConfigReader.read(filepath)
+    return read_default_params("snakemake")
